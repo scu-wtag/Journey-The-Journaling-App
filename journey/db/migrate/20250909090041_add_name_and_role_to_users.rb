3a2b8143f8b92fd0b09c48ef941b6ed30dbe5e29
scu-wtag <sophia.cuarte@welldev.io>
2025-09-09 16:30:57 +0200

    Update 20250909090041_add_name_and_role_to_users.rb

--- journey/db/migrate/20250909090041_add_name_and_role_to_users.rb
+++ journey/db/migrate/20250909090041_add_name_and_role_to_users.rb
@@ -1,7 +1,7 @@
 class AddNameAndRoleToUsers < ActiveRecord::Migration[7.1]
   def change
     add_column :users, :name, :string
-    add_column :users, :role, :string, null: false, default: "member"
+    add_column :users, :role, :integer, null: false, default: 1
     add_index  :users, :role
   end
 end
