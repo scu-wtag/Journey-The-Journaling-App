0db28e85d38dc8ee7ddb02b129158c137e897c6e
scu-wtag <sophia.cuarte@welldev.io>
2025-09-11 11:37:38 +0200

    Update journey/app/models/user.rb
    Co-authored-by: Evea Zerin Khan <90237465+evk-wtag@users.noreply.github.com>

--- journey/app/models/user.rb
+++ journey/app/models/user.rb
@@ -2,14 +2,18 @@ class User < ApplicationRecord
   include Clearance::User
 
   has_one :profile, dependent: :destroy
+  
   accepts_nested_attributes_for :profile
 
-  validates :name, presence: true
-
   enum :role, { guest: 0, member: 1, admin: 2 }, default: :member
+  
   validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :name, presence: true 
+  
   before_save :downcase_email
+  
   private
+ 
 
   def downcase_email
     self.email = email.downcase if email.present?
