6bb85c880a2460bab0a2cbc18bc2144d293cbd41
scu-wtag <sophia.cuarte@welldev.io>
2025-09-11 13:39:00 +0200

    Sign in: custom SessionsController, routes, i18n (rebased on sign-up)

--- journey/config/routes.rb
+++ journey/config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  resources :users, controller: 'users', only: %i(new create)
+  get  '/signup', to: 'users#new', as: :sign_up
+  post '/signup', to: 'users#create'
+
+  resource :session, controller: 'sessions', only: %i(create destroy)
+  get    '/login',  to: 'sessions#new', as: :sign_in
+  post   '/login',  to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy', as: :sign_out
+
   root 'home#show'
 
   # Sign up
