389580a3468e25f8bdd69ca294e894cd666b3854
scu-wtag <sophia.cuarte@welldev.io>
2025-09-11 13:39:00 +0200

    Sign in: custom SessionsController, routes, i18n (rebased on sign-up)

--- journey/app/controllers/sessions_controller.rb
+++ journey/app/controllers/sessions_controller.rb
@@ -2,9 +2,7 @@ class SessionsController < Clearance::SessionsController
   private
 
   def url_after_create
-    if signed_in? && current_user.respond_to?(:teams)
-      session[:team_ids] = current_user.teams.pluck(:id)
-    end
+    session[:team_ids] = current_user.teams.pluck(:id) if signed_in? && current_user.respond_to?(:teams)
     root_path
   end
 
