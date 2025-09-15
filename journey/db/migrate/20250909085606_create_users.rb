754d72aa7aa0f0accfda63dfc846d01bc05d558e
scu-wtag <sophia.cuarte@welldev.io>
2025-09-15 09:13:10 +0200

    Update journey/db/migrate/20250909085606_create_users.rb
    Co-authored-by: Evea Zerin Khan <90237465+evk-wtag@users.noreply.github.com>

--- journey/db/migrate/20250909085606_create_users.rb
+++ journey/db/migrate/20250909085606_create_users.rb
@@ -4,17 +4,17 @@ class CreateUsers < ActiveRecord::Migration[7.1]
       t.string  :email, null: false
       t.string  :encrypted_password, null: false
       t.string  :confirmation_token
-      t.string  :remember_token,     null: false
+      t.string  :remember_token, null: false
 
-      t.string  :name,               null: false
-      t.integer :role,               null: false, default: 1
+      t.string  :name, null: false
+      t.integer :role, null: false, default: 1
 
       t.timestamps
     end
 
-    add_index :users, :email,              unique: true
+    add_index :users, :email, unique: true
     add_index :users, :confirmation_token, unique: true
-    add_index :users, :remember_token,     unique: true
+    add_index :users, :remember_token, unique: true
     add_index :users, :role
   end
 end
