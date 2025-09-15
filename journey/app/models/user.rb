071116a28687eeab158131404b174b14fc4c9897
scu-wtag <sophia.cuarte@welldev.io>
2025-09-15 10:57:38 +0200

    Update journey/app/models/user.rb
    Co-authored-by: Md. Ahnaf Muttaqi Chowdhury <ahnaf.muttaqi@welldev.io>

--- journey/app/models/user.rb
+++ journey/app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   accepts_nested_attributes_for :profile
 
   validates :name,  presence: true
-  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: URI::MailTo::EMAIL_REGEXP }
 
   enum :role, { guest: 0, member: 1, admin: 2 }, default: :member
 
