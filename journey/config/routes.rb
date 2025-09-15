01f0e99202cd2a20aecdcb787602e1afcc8ce1ff
scu-wtag <sophia.cuarte@welldev.io>
2025-09-15 12:17:44 +0200

    rebased

--- journey/config/routes.rb
+++ journey/config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
-  resources :users, controller: "users", only: [ :new, :create ]
-  post "/signup", to: "users#create"
+  resources :users, controller: 'users', only: %i(new create)
+  post '/signup', to: 'users#create'
 
-  resource :session, controller: "sessions", only: [ :create, :destroy ]
-  get    "/login",  to: "sessions#new",     as: :sign_in
-  post   "/login",  to: "sessions#create"
-  delete "/logout", to: "sessions#destroy", as: :sign_out
+  resource :session, controller: 'sessions', only: %i(create destroy)
+  get    '/login',  to: 'sessions#new', as: :sign_in
+  post   '/login',  to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy', as: :sign_out
 
+  root 'home#show'
+
+  get  'signup', to: 'users#new', as: :sign_up
+  post   'session', to: 'sessions#create'
 
   resource :profile, only: %i(show edit update)
 end
