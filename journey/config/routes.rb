a47416d2efa90d74a51bfd49b6a907f8b44d54ed
scu-wtag <sophia.cuarte@welldev.io>
2025-09-16 08:12:57 +0200

    Update journey/config/routes.rb
    Co-authored-by: Md. Ahnaf Muttaqi Chowdhury <ahnaf.muttaqi@welldev.io>

--- journey/config/routes.rb
+++ journey/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :users, only: %i[new create]
   post '/signup', to: 'users#create'
 
-  resource :session, controller: 'sessions', only: %i(create destroy)
+  resource :session, only: %i[create destroy]
   get    '/login',  to: 'sessions#new', as: :sign_in
   post   '/login',  to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy', as: :sign_out
