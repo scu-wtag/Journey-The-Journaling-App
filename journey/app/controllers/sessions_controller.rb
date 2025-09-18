cf94aa313ffbe46fddad6c0eb03e577fde6d6da9
scu-wtag <sophia.cuarte@welldev.io>
2025-09-18 14:20:35 +0200

    tests and small details also rubocop

--- journey/app/controllers/sessions_controller.rb
+++ journey/app/controllers/sessions_controller.rb
@@ -1,4 +1,20 @@
 class SessionsController < Clearance::SessionsController
+  def new
+    @session = SessionForm.new(email: params.dig(:session, :email))
+  end
+
+  def create
+    @session = SessionForm.new(session_params)
+
+    if (user = @session.authenticate)
+      sign_in user
+      redirect_to root_path, notice: t('sessions.success')
+    else
+      flash.now[:alert] = t('sessions.errors.wrong_email_or_password')
+      render :new, status: :unprocessable_content
+    end
+  end
+
   private
 
   def url_after_create
@@ -8,5 +24,9 @@ class SessionsController < Clearance::SessionsController
 
   def url_after_destroy
     login_path
+  end
+
+  def session_params
+    params.expect(session: %i(email password))
   end
 end
---
+++ journey/app/forms/session_form.rb
@@ -0,0 +1,20 @@
+class SessionForm
+  include ActiveModel::Model
+
+  attr_accessor :email, :password
+
+  validates :email, :password, presence: true
+
+  def authenticate
+    return nil unless valid?
+
+    user = User.find_by(email: email)
+    ok = if user.respond_to?(:authenticate)
+           user.authenticate(password)
+         elsif user.respond_to?(:authenticated?)
+           user.authenticated?(password) && user
+         end
+    errors.add(:base, I18n.t('sessions.errors.wrong_email_or_password')) unless ok
+    ok
+  end
+end
