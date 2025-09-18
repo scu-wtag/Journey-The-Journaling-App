6161fb8a71bd909b5c12f95bb021cf7e0bc45150
scu-wtag <sophia.cuarte@welldev.io>
2025-09-18 15:09:58 +0200

    rubocop

--- journey/app/controllers/registrations_controller.rb
+++ journey/app/controllers/registrations_controller.rb
@@ -1,5 +1,5 @@
 class RegistrationsController < Clearance::UsersController
-  before_action :build_user,   only: :new
+  before_action :build_user, only: :new
   before_action :prepare_user, only: :create
 
   def new; end
@@ -9,7 +9,7 @@ class RegistrationsController < Clearance::UsersController
     normalize_phone!(@user.profile)
 
     return render_new_error if @user.errors.any?
-    return handle_success   if @user.save
+    return handle_success if @user.save
 
     handle_failure
   end
@@ -27,20 +27,20 @@ class RegistrationsController < Clearance::UsersController
   end
 
   def user_from_params
-    attrs = params.require(:user).permit(
-      :email, :password, :name,
-      profile_attributes: %i[phone_country_code phone_local phone birthday country headquarters]
+    attrs = params.expect(
+      user: [:email, :password, :name,
+             { profile_attributes: %i(phone_country_code phone_local phone birthday country headquarters) },],
     )
     User.new(attrs)
   end
 
   def user_params
-    params.require(:user).permit(:email, :password, :name,
-      profile_attributes: %i[phone_country_code phone_local phone birthday country headquarters])
+    params.expect(user: [:email, :password, :name,
+                         { profile_attributes: %i(phone_country_code phone_local phone birthday country headquarters) },])
   end
 
   def check_password_confirmation
-    password  = params.dig(:user, :password)
+    password = params.dig(:user, :password)
     conformation = params.dig(:user, :password_confirmation)
     return if conf.blank? || password == conformation
 
@@ -49,8 +49,9 @@ class RegistrationsController < Clearance::UsersController
 
   def normalize_phone!(profile)
     return unless profile
-    code  = profile.phone_country_code.to_s.strip
-    local = profile.phone_local.to_s.gsub(/\D/, "")
+
+    code = profile.phone_country_code.to_s.strip
+    local = profile.phone_local.to_s.gsub(/\D/, '')
     profile.phone = code.present? && local.present? ? "+#{code}#{local}" : nil
   end
 
@@ -59,15 +60,15 @@ class RegistrationsController < Clearance::UsersController
   end
 
   def handle_success
-    redirect_to Clearance.configuration.redirect_url, notice: t("users.create.success")
+    redirect_to Clearance.configuration.redirect_url, notice: t('users.create.success')
   end
 
   def handle_failure
-    flash.now[:alert] = t("users.create.failed", default: "")
+    flash.now[:alert] = t('users.create.failed', default: '')
     render :new, status: :unprocessable_content
   end
 
-  prepend_before_action :require_signed_out, only: %i[new create]
+  prepend_before_action :require_signed_out, only: %i(new create)
   def require_signed_out
     redirect_to root_path(locale: I18n.locale) if signed_in?
   end
