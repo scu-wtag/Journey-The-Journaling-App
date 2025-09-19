5039a621266d1bcf966c38fe3ac24fa713b36126
scu-wtag <sophia.cuarte@welldev.io>
2025-09-19 11:18:16 +0200

    added the sign up page visual

--- journey/app/helpers/application_helper.rb
+++ journey/app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def field_errors_for(record, attr)
+    return "" unless record&.errors&.[](attr)&.any?
+    content_tag :p, record.errors[attr].first, class: "field-error", role: "alert"
+  end
+
+  def field_invalid?(record, attr)
+    record&.errors&.[](attr)&.any?
+  end
 end
