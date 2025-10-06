afb5171607514f88e7afb9cb6f34c4980d11fe79
scu-wtag <sophia.cuarte@welldev.io>
2025-10-06 09:36:36 +0200

    adjusting i18n and fixing an error

--- journey/app/models/concerns/profile_options.rb
+++ journey/app/models/concerns/profile_options.rb
@@ -1,22 +1,23 @@
 module ProfileOptions
   extend ActiveSupport::Concern
 
+  HEADQUARTERS = %w[ZRH DAC REM].freeze
+
   class_methods do
     def phone_country_options
       ISO3166::Country.all
-        .filter_map do |country|
-          code = country.country_code
-          next if code.blank?
+      .filter_map do |country|
+        code = country.country_code
+        next if code.blank?
 
-          name =
-            country.translation(I18n.locale.to_s) ||
-            country.common_name ||
-            country.iso_short_name ||
-            country.name
+        name = country.translation(I18n.locale.to_s) ||
+        country.common_name ||
+        country.iso_short_name ||
+        country.name
 
-          ["#{name} (+#{code})", code]
-        end
-        .sort_by(&:first)
+        ["#{name} (+#{code})", code]
+      end
+      .sort_by(&:first)
     end
 
     def headquarter_options
--- journey/app/models/profile.rb
+++ journey/app/models/profile.rb
@@ -1,13 +1,18 @@
 class Profile < ApplicationRecord
+  include ProfileOptions
+
   belongs_to :user, inverse_of: :profile
   validates :user_id, uniqueness: true
+
   has_one_attached :picture
+  validate :picture_type_and_size
+
   before_validation :compose_phone
 
   private
 
   def compose_phone
-    code = phone_country_code.to_s.strip
+    code  = phone_country_code.to_s.strip
     local = phone_local.to_s.gsub(/\D/, '')
     self.phone = code.present? && local.present? ? "+#{code}#{local}" : nil
   end
@@ -15,16 +20,13 @@ class Profile < ApplicationRecord
   def picture_type_and_size
     return unless picture.attached?
 
-    errors.add(:picture, t('profile.update.failed', default: '')) if picture.blob.byte_size > 5.megabytes
+    if picture.blob.byte_size > 5.megabytes
+      errors.add(:picture, I18n.t('profiles.update.failed', default: ''))
+    end
 
     acceptable = %w(image/jpeg image/png image/webp image/avif)
     return if acceptable.include?(picture.blob.content_type)
 
-    errors.add(:picture, t('profile.update.wrong_filetype', default: ''))
+    errors.add(:picture, I18n.t('profiles.update.wrong_filetype', default: ''))
   end
-  include ProfileOptions
-
-  belongs_to :user
-
-  HEADQUARTERS = %w[ZRH DAC REM].freeze
 end
