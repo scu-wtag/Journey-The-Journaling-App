abb457499eb035b6619fe6839df0eb5bd625b31d
scu-wtag <sophia.cuarte@welldev.io>
2025-10-09 09:01:02 +0200

    fixing some errors

--- journey/config/initializers/dartsass.rb
+++ journey/config/initializers/dartsass.rb
@@ -3,6 +3,7 @@ Rails.application.config.dartsass.builds = {
   'views/sessions/new.scss' => 'views/sessions/new.css',
   'views/users/new.scss' => 'views/users/new.css',
   'views/profiles/show.scss' => 'views/profiles/show.css',
+  'views/journals/new.scss' => 'views/journals/new.css'
 }
 
 Rails.application.config.dartsass.build_options = [
