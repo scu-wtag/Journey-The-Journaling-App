d7078bed6c594b5c643cee88d0e2309ba32516d5
scu-wtag <sophia.cuarte@welldev.io>
2025-10-29 10:18:52 +0100

    final changes

--- journey/config/initializers/dartsass.rb
+++ journey/config/initializers/dartsass.rb
@@ -1,5 +1,6 @@
 Rails.application.config.dartsass.builds = {
   'application.scss' => 'application.css',
+  'theme_overrides.scss' => 'theme_overrides.css',
   'views/sessions/new.scss' => 'views/sessions/new.css',
   'views/users/new.scss' => 'views/users/new.css',
   'views/profiles/show.scss' => 'views/profiles/show.css',
